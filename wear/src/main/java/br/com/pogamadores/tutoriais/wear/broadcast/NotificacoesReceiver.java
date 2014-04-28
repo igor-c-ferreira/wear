1e9259635099be2547f6e864f53386a5c9432e40
Igor Ferreira <igorcferreira@hotmail.com>
2014-04-28 15:51:54 -0300

    Criacao das classes de suporte

---
+++ wear/src/main/java/br/com/pogamadores/tutoriais/wear/broadcast/NotificacoesReceiver.java
@@ -0,0 +1,17 @@
+package br.com.pogamadores.tutoriais.wear.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificacoesReceiver extends BroadcastReceiver {
+    public NotificacoesReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
