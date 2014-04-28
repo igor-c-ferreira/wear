cc2a1bc046d4a2b104cb5980f5158231d110b857
Igor Ferreira <igorcferreira@hotmail.com>
2014-04-28 15:40:08 -0300

    - Initial commit

---
+++ wear/src/main/java/br/com/pogamadores/tutoriais/wear/ui/fragment/HomeFragment.java
@@ -0,0 +1,20 @@
+package br.com.pogamadores.tutoriais.wear.ui.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.com.pogamadores.tutoriais.wear.R;
+
+public class HomeFragment extends Fragment
+{
+    public HomeFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        return rootView;
+    }
+}
